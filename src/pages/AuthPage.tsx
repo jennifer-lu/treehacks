6e427fd55efe241bf7a8e3ae63caa4639377df1f
Boya <boyazhang123@gmail.com>
2023-02-18 11:48:55 -0800

    setup supabase cloud function example

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import { Flex, Heading } from '@chakra-ui/react'
+import React, { useEffect } from 'react';
+import { Flex, Heading } from '@chakra-ui/react';
+import { supabase } from '../lib/api';
+
+
 
 
 const AuthPage = () => {
-    return (
-        <Flex direction="column" align="center"><Heading>platedate</Heading></Flex>
-    )
+  useEffect(() => {
+    async function fetchHelloWorld() {
+      try {
+        const { data, error } = await supabase.functions.invoke('hello-world', {
+          body: { name: 'Functions' },
+        });
+        console.log(data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchHelloWorld();
+  }, []);
+
+  return (
+    <Flex direction="column" align="center">
+      <Heading>platedate</Heading>
+    </Flex>
+  );
 };
 
 export default AuthPage;
---
+++ src/pages/NewProfilePage.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { Flex, Heading } from '@chakra-ui/react';
+
+const AuthPage = () => {
+  return (
+    <Flex direction="column" align="center">
+      <Heading>platedate</Heading>
+    </Flex>
+  );
+};
+
+export default AuthPage;
