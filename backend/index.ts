3e09e527e3bdb0884316a9cbc067e750fe10d0a8
Boya <boyazhang123@gmail.com>
2023-02-19 00:19:57 -0800

    add login auth

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,9 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Express, Request, Response } from 'express';
+import { Prisma, PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const CORS_ALLOW_LIST = ['http://localhost:3000'];
 
 const CORS_OPTIONS = {
@@ -19,7 +21,31 @@ app.get('/hi', (req: Request, res: Response) => {
   res.json('Hello World');
 });
 
-app.listen({ port: 5000 }, () => {
+// create user
+app.get('/login', async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+      select: { id: true, password: true },
+    });
+
+    if (user.password !== password) {
+      return res.status(401).json({ error: 'Invalid Password' });
+    }
+    res.json({ userId: user.id });
+  } catch (e) {
+    res
+      .status(500)
+      .json({ error: `User with email ${email} does not exist in database` });
+  }
+});
+
+//
+app.post('/signup', async (req: Request, res: Response) => {
+  
+});
+app.app.listen({ port: 5000 }, () => {
   /* eslint-disable-next-line no-console */
   console.info(`Server is listening on port 5000}!`);
 });
